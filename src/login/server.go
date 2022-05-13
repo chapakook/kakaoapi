ee8ec073c23bc436522645c916070dd412f08f0b
chapakook <chapakook96@gmail.com>
2022-05-13 17:19:38 +0900

    start doing

--- src/login/server.go
+++ src/login/server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/gofiber/fiber/v2"
@@ -10,23 +11,42 @@ import (
 )
 
 var (
-	apiKey string
+	apiKey      string
+	baseUrl     string
+	redirectUrl string
 )
 
 func main() {
 	initEnv()
+	getAuthorize()
+	getToken()
 	app := fiber.New()
+	// Load static file like CSS, Images, JS ...
+	app.Static("/public", "./public")
+
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("hello world")
 	})
 	log.Fatal(app.Listen(":3000"))
 }
 
+func getToken() {
+}
+
+func getAuthorize() {
+	url := fmt.Sprintf("%s/oauth/authorize?client_id=%s&redirect_uri=%s&response_type=code", baseUrl, apiKey, redirectUrl)
+	//url := fmt.Sprintf("%s/oauth/authorize?client_id=%s&redirect_uri=%s&response_type=code&scope=account_emai", baseUrl, apiKey, redirectUrl)
+	res, err := http.Get(url)
+	checkErr(err, "Error Token check code plz!!!")
+	fmt.Println(res)
+}
+
 func initEnv() {
 	eErr := godotenv.Load()
 	checkErr(eErr, "Error loading .env file")
 	apiKey = os.Getenv("REST_API_KEY")
-	fmt.Println(apiKey)
+	baseUrl = os.Getenv("BASE_URL")
+	redirectUrl = os.Getenv("REDIRECT_URI")
 }
 
 func checkErr(err error, msg string) {
