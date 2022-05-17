085d032e92366f6652be7d2a97dda027d327b4f4
chapakook <chapakook96@gmail.com>
2022-05-17 12:02:20 +0900

    html create

--- src/login/go.mod
+++ src/login/go.mod
@@ -9,10 +9,15 @@ require (
 	github.com/gofiber/jwt/v3 v3.2.11 // indirect
 	github.com/gofiber/template v1.6.27 // indirect
 	github.com/golang-jwt/jwt/v4 v4.4.1 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/joho/godotenv v1.4.0 // indirect
 	github.com/klauspost/compress v1.15.4 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.36.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/net v0.0.0-20220425223048-2871e0cb64e4 // indirect
+	golang.org/x/oauth2 v0.0.0-20220411215720-9780585627b5 // indirect
 	golang.org/x/sys v0.0.0-20220503163025-988cb79eb6c6 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
 )
--- src/login/main.go
+++ src/login/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -9,18 +10,21 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/template/html"
 	"github.com/joho/godotenv"
+	"golang.org/x/oauth2"
 )
 
 var (
-	apiKey      string
-	baseUrl     string
-	redirectUrl string
+	apiKey       string
+	baseUrl      string
+	redirectUrl  string
+	clientID     string
+	clientSecret string
+	OAuthConf    *oauth2.Config
 )
 
 func main() {
 	initEnv()
-	getAuthorize()
-	getToken()
+	initOAuth()
 
 	// Load html
 	engine := html.New("./views", ".html")
@@ -38,30 +42,74 @@ func main() {
 		})
 	})
 
+	app.Get("/oauth/callback", func(c *fiber.Ctx) error {
+		return c.Render("kakao_login", nil)
+	})
+
+	api := app.Group("/api")
+
+	v1 := api.Group("/v1")
+	v1.Get("/login", func(c *fiber.Ctx) error {
+		kakaoLogin()
+		return c.SendString("Get /api/v1/login")
+	})
+
 	log.Fatal(app.Listen(":3000"))
 }
 
-func callKakaoLogin() {
-	fmt.Println("Called")
+func initOAuth() {
+	OAuthConf = &oauth2.Config{
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+		RedirectURL:  redirectUrl,
+	}
 }
 
-func getToken() {
+func kakaoLogin() {
+	getOAuthorize()
+	getToken()
 }
 
-func getAuthorize() {
+func getToken() error {
+	fmt.Println("getToken")
+	return nil
+}
+
+func getOAuthorize() error {
 	url := fmt.Sprintf("%s/oauth/authorize?client_id=%s&redirect_uri=%s&response_type=code", baseUrl, apiKey, redirectUrl)
 	//url := fmt.Sprintf("%s/oauth/authorize?client_id=%s&redirect_uri=%s&response_type=code&scope=account_emai", baseUrl, apiKey, redirectUrl)
 	res, err := http.Get(url)
-	checkErr(err, "Error Token check code plz!!!")
-	fmt.Println(res)
+	//fmt.Println(url)
+	//fmt.Println(res)
+	if err != nil {
+		return err
+	}
+
+	data, dErr := ioutil.ReadAll(res.Body)
+	if dErr != nil {
+		return dErr
+	}
+
+	file, cErr := os.Create("views/kakao_login.html")
+	if cErr != nil {
+		return cErr
+	}
+	_, wErr := file.Write([]byte(data))
+	if wErr != nil {
+		return wErr
+	}
+	return nil
 }
 
 func initEnv() {
 	eErr := godotenv.Load()
 	checkErr(eErr, "Error loading .env file")
+
 	apiKey = os.Getenv("REST_API_KEY")
 	baseUrl = os.Getenv("BASE_URL")
 	redirectUrl = os.Getenv("REDIRECT_URI")
+	clientID = os.Getenv("Client_ID")
+	clientSecret = os.Getenv("Client_Secret")
 }
 
 func checkErr(err error, msg string) {
