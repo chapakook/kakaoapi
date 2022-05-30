35bead71a5db5bf49706af0693d4c46e616cbf7b
chapakook <chapakook96@gmail.com>
2022-05-30 16:29:31 +0900

    REST API Login Update

--- src/login/controller.go
+++ src/login/controller.go
@@ -22,7 +22,6 @@ func Index(c *fiber.Ctx) error {
 }
 
 func OAuth(c *fiber.Ctx) error {
-	// front-end -(query)-> back-end
 	code := c.Query("code")
 
 	// retrieve token from user code
@@ -32,27 +31,23 @@ func OAuth(c *fiber.Ctx) error {
 		"redirect_uri": []string{REDIRECT_URI},
 		"code":         []string{code},
 	})
-	if err != nil {
-		panic(err)
-	}
+	CheckErr(err)
+	CheckStatus(resp)
 
-	// decode Response JSON
 	var auth KakaoAuthResult
 	err = json.NewDecoder(resp.Body).Decode(&auth)
-	if err != nil {
-		panic(err)
-	}
+	CheckErr(err)
 
 	// add cookies
 	cookie := new(fiber.Cookie)
 	cookie.Name = "accesstoken"
 	cookie.Value = auth.AccessToken
-	cookie.Expires = time.Now().Add(24 * time.Hour)
+	cookie.Expires = time.Now().Add(time.Duration(auth.ExpiresIn) * time.Second)
 	c.Cookie(cookie)
 
 	cookie.Name = "refreshtoken"
 	cookie.Value = auth.RefreshToken
-	cookie.Expires = time.Now().Add(24 * time.Hour)
+	cookie.Expires = time.Now().Add(time.Duration(auth.RefreshTokenExpiresIn) * time.Second)
 	c.Cookie(cookie)
 
 	// print info's in console
@@ -75,22 +70,18 @@ func OAuth(c *fiber.Ctx) error {
 }
 
 func Info(c *fiber.Ctx) error {
-	client := http.Client{}
 	req, err := http.NewRequest("GET", BASE_API_URL+"/v1/user/access_token_info", nil)
-	if err != nil {
-		panic(err)
-	}
+	CheckErr(err)
 	req.Header.Add("Authorization", "Bearer "+c.Cookies("accesstoken"))
+
+	client := http.Client{}
 	resp, err := client.Do(req)
-	if err != nil {
-		panic(err)
-	}
+	CheckErr(err)
+	CheckStatus(resp)
 
 	var tokenInfo AccessTokenInfo
 	err = json.NewDecoder(resp.Body).Decode(&tokenInfo)
-	if err != nil {
-		panic(err)
-	}
+	CheckErr(err)
 
 	// print info's in console
 	fmt.Println("[+] Token Info")
@@ -114,16 +105,12 @@ func Refresh(c *fiber.Ctx) error {
 		"client_id":     []string{REST_API_CLIENT_KEY},
 		"refresh_token": []string{c.Cookies("refreshtoken")},
 	})
-	if err != nil {
-		panic(err)
-	}
+	CheckErr(err)
+	CheckStatus(resp)
 
-	// decode Response JSON
 	var auth KakaoAuthResult
 	err = json.NewDecoder(resp.Body).Decode(&auth)
-	if err != nil {
-		panic(err)
-	}
+	CheckErr(err)
 
 	if auth.RefreshToken == "" {
 		auth.RefreshToken = c.Cookies("refreshtoken")
@@ -161,28 +148,23 @@ func Refresh(c *fiber.Ctx) error {
 }
 
 func Scopes(c *fiber.Ctx) error {
-	client := http.Client{}
 	req, err := http.NewRequest("GET", BASE_API_URL+"/v2/user/scopes", nil)
-	if err != nil {
-		panic(err)
-	}
+	CheckErr(err)
 	req.Header.Add("Authorization", "Bearer "+c.Cookies("accesstoken"))
+
+	client := http.Client{}
 	resp, err := client.Do(req)
-	if err != nil {
-		panic(err)
-	}
+	CheckErr(err)
+	CheckStatus(resp)
 
 	var scopes ScopeResult
 	err = json.NewDecoder(resp.Body).Decode(&scopes)
-	if err != nil {
-		panic(err)
-	}
+	CheckErr(err)
 
 	// print info's in console
 	fmt.Println("[+] Scope Result")
 	fmt.Println("	ID : ", scopes.ID)
 
-	id := ""
 	for i, s := range scopes.Scopes {
 		fmt.Println("	Scopes - ", i)
 		fmt.Println("	  - Agreed : ", s.Agreed)
@@ -191,121 +173,35 @@ func Scopes(c *fiber.Ctx) error {
 		fmt.Println("	  - Revocable : ", s.Revocable)
 		fmt.Println("	  - Type : ", s.Type)
 		fmt.Println("	  - Using : ", s.Using)
-		id += s.ID + "|"
 	}
 
 	return c.Render("scopes", fiber.Map{
 		"Title":    "Get Scopes Success!!",
 		"SubTitle": "Step 5 - Logout",
 		"Scopes":   scopes.Scopes,
-		"ID":       id,
 	})
 }
 
-// error disable
-// func Revoke(c *fiber.Ctx) error {
-// 	tmp := c.Query("id")
-// 	var id []string
-// 	for tmp != "" {
-// 		n := strings.Index(tmp, "|")
-// 		id = append(id, "\""+tmp[:n]+"\"")
-// 		arr := tmp[n+1:]
-// 		tmp = arr
-// 	}
-
-// 	params := url.Values{}
-// 	str := strings.Join(id, ",")
-// 	str = "[" + str + "]"
-// 	params.Add("scopes", str)
-// 	fmt.Println(str)
-
-// 	client := http.Client{}
-// 	req, err := http.NewRequest("POST", BASE_API_URL+"/v2/user/revoke/scopes", bytes.NewBufferString(params.Encode()))
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	req.Header.Add("Authorization", "Bearer "+c.Cookies("accesstoken"))
-// 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-
-// 	resp, err := client.Do(req)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	var revoke RevokeResult
-// 	err = json.NewDecoder(resp.Body).Decode(&revoke)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	// print info's in console
-// 	fmt.Println("[+] Revoke Result")
-// 	fmt.Println("	TargetID : ", revoke.TargetID)
-// 	fmt.Println("	TargetIDType : ", revoke.TargetIDType)
-// 	for i, s := range revoke.Scopes {
-// 		fmt.Println("	Scopes - ", i)
-// 		fmt.Println("	  - Agreed : ", s.Agreed)
-// 		fmt.Println("	  - DisplayName : ", s.DisplayName)
-// 		fmt.Println("	  - ID : ", s.ID)
-// 		fmt.Println("	  - Revocable : ", s.Revocable)
-// 		fmt.Println("	  - Type : ", s.Type)
-// 		fmt.Println("	  - Using : ", s.Using)
-// 	}
-
-// 	return c.Render("revoke", fiber.Map{
-// 		"Title":        "Revoke Success!!",
-// 		"SubTitle":     "Step 6 - Logout",
-// 		"TargetID":     revoke.TargetID,
-// 		"TargetIDType": revoke.TargetIDType,
-// 		"Scopes":       revoke.Scopes,
-// 	})
-// }
-
 func Logout(c *fiber.Ctx) error {
-	client := http.Client{}
-
-	// logout
 	req, err := http.NewRequest("POST", BASE_API_URL+"/v1/user/logout", nil)
-	if err != nil {
-		panic(err)
-	}
+	CheckErr(err)
 	req.Header.Add("Authorization", "Bearer "+c.Cookies("accesstoken"))
+
+	client := http.Client{}
 	resp, err := client.Do(req)
-	if err != nil {
-		panic(err)
-	}
+	CheckErr(err)
+	CheckStatus(resp)
 
 	var logoutinfo LogoutInfo
 	err = json.NewDecoder(resp.Body).Decode(&logoutinfo)
-	if err != nil {
-		panic(err)
-	}
+	CheckErr(err)
 
 	// print info's in console
 	fmt.Println("[+] Logout")
 	fmt.Println("	ID : ", logoutinfo.ID)
 
-	// unlink
-	req, err = http.NewRequest("POST", BASE_API_URL+"/v1/user/unlink", nil)
-	if err != nil {
-		panic(err)
-	}
-	req.Header.Add("Authorization", "Bearer "+c.Cookies("accesstoken"))
-	resp, err = client.Do(req)
-	if err != nil {
-		panic(err)
-	}
-
-	err = json.NewDecoder(resp.Body).Decode(&logoutinfo)
-	if err != nil {
-		panic(err)
-	}
-
 	// cookies clear
 	c.ClearCookie()
-
-	// print info's in console
-	fmt.Println("[+] Unlink")
-	fmt.Println("	ID : ", logoutinfo.ID)
 
 	return c.Redirect("http://localhost:3000/end?id=" + strconv.FormatUint(logoutinfo.ID, 10))
 }
